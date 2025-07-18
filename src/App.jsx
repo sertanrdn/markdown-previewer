cbf27f29bf2c79e2d91b8018bb41ccbd9385da70
sertanrdn <sertanerdogan07@gmail.com>
2025-07-18 19:38:30 +0200

    Added components to main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
+
+const initialMarkdown = `# Hello Markdown!
+  ## Sub-heading
+  [Link](https://example.com)
+  \`inline code\`
+
+  \`\`\`
+  code block
+  \`\`\`
+
+  - List item
+  > Blockquote
+
+  **Bold**
+
+  ![Image](https://placekitten.com/100/100)
+`;
 
 function App() {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleChange = (e) => setMarkdown(e.target.value);
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Editor markdown={markdown} onChange={handleChange} />
+      <Preview markdown={markdown} />
+    </div>
   )
 }
 
